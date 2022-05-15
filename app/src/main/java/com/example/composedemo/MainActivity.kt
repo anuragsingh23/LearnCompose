ade5ccb3e7b1e8af89b66cb36d8e973494ba2af5
anuragsingh23 <anuragshekhawat17@gmail.com>
2022-05-15 23:08:56 +0530

    This branch explain how to use @Row and @Column (Layout basically)

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -4,50 +4,69 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import com.example.composedemo.ui.theme.ComposeDemoTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                Text()
+            LayoutCompose()
         }
     }
 }
-@Composable
-fun Text(
-){
-    val context = LocalContext.current
-    Text(
-         text = "Anurag Shekhawat",
-         fontSize = 45.sp,
-         fontFamily = FontFamily.Cursive ,
-         color = Color.Green ,
-         modifier = Modifier.clickable {
-             Toast.makeText(context,"You got me ",Toast.LENGTH_LONG).show()
-         }
 
-    )
+@Composable
+fun LayoutCompose() {
+    val context = LocalContext.current
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentHeight()
+        .padding(12.dp)
+        .border(width = 1.dp, color = Color.LightGray)
+        .padding(12.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.pic),
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(120.dp)
+                .clip(CircleShape)
+                .rotate(270F)
+        )
+        Column() {
+            Text(text = stringResource(id = R.string.name))
+            Button(onClick = {
+                Toast.makeText(context, "hello handsome", Toast.LENGTH_SHORT).show()
+            }) {
+                Text(text = "Click me")
+            }
+        }
+    }
+
 }
+
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview(){
-    Surface (modifier = Modifier.fillMaxSize()){
-        Text()
+fun DefaultPreview() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        LayoutCompose()
     }
 }
