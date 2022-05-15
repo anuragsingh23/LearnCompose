e3350b966c17b7e16f115868c192dd6761931d21
anuragsingh23 <anuragshekhawat17@gmail.com>
2022-05-16 00:15:25 +0530

    listview predefined

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -1,53 +1,91 @@
 package com.example.composedemo
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import com.example.composedemo.ui.theme.ComposeDemoTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                Text()
+            UserList()
         }
     }
 }
 @Composable
-fun Text(
+fun UserList(
 ){
-    val context = LocalContext.current
-    Text(
-         text = "Anurag Shekhawat",
-         fontSize = 45.sp,
-         fontFamily = FontFamily.Cursive ,
-         color = Color.Green ,
-         modifier = Modifier.clickable {
-             Toast.makeText(context,"You got me ",Toast.LENGTH_LONG).show()
-         }
+    Column( modifier = Modifier.verticalScroll(rememberScrollState())
+    ) {
+        for (i in 1..10){
+            UserCard()
+    }
+    }
 
-    )
 }
+
+@Composable
+fun UserCard(){
+    Card(
+        elevation = 4.dp,
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(12.dp)
+                .padding(12.dp)
+
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(120.dp)
+                    .clip(CircleShape)
+            )
+            Column() {
+                Text(text = stringResource(id = R.string.app_name))
+                Button(onClick = {
+                    //Button action
+                }) {
+                    Text(text = "View Profile")
+                }
+
+            }
+        }
+    }
+
+}
+
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview(){
-    Surface (modifier = Modifier.fillMaxSize()){
-        Text()
-    }
+fun UserCAseCard(){
+        UserCard()
 }
